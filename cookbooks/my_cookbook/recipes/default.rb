602c999e2eed781df29664534c60c7bf8d29486d
earlwaud <earl.waud@gmail.com>
2015-01-19 14:35:17 -0800

    More changes

--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -3,6 +3,7 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-include_recipe "chef-client"
-include_recipe "apt"
-include_recipe "ntp"
+
+template '/tmp/greeting.txt' do
+  variables greeting: 'Hello!'
+end
